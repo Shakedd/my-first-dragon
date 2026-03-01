f26851e90084f493ff876d4934e99c7bdecc610c
Shaked <shakeduzan77@gmail.com>
2026-03-01 15:54:09 +0200

    add funcs eat(), sleep(), play()

--- my_first_dragon.py
+++ my_first_dragon.py
@@ -16,3 +16,27 @@ class Pet():
         self.energy = energy
         self.points = (hunger + happiness + energy)/3
         self.history = history
+        
+    def points_update(self):
+        self.points = (self.hunger + self.happiness + self.energy)/3
+        
+    def eat(self):
+        self.hunger = 0
+        if (self.energy + 20) > 100:
+            self.energy = 100
+        else:
+            self.energy += 20
+    
+    def sleep(self):
+        self.energy = 100
+        if (self.hunger - 20) < 0:
+            self.hunger = 0
+        else:
+            self.hunger -= 20
+    
+    def play(self):
+        self.happiness = 100
+        if (self.energy -20) < 0:
+            self.energy = 0
+        else:
+            self.energy -= 20
