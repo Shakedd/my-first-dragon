b122150747ed84bb0abbaba0db027328f8410cad
Shaked <shakeduzan77@gmail.com>
2026-03-01 15:11:12 +0200

    add class Pet_Type and Pet

--- my_first_dragon.py
+++ my_first_dragon.py
@@ -1,1 +1,18 @@
-print("hello world")
+from enum import Enum
+
+class Pet_Type(Enum):
+    Bear,
+    Deer,
+    Hourse
+
+class Pet():
+    DEFAULT_PATH = "history.txt"
+    
+    def __init__(name: str, type: Pet_Type, hunger: int = 50, happiness: int = 50, energy: int = 50, history: str = DEFAULT_PATH):
+        self.name = name
+        self.type = type
+        self.hunger = hunger
+        self.happiness = happiness
+        self.energy = energy
+        self.points = (hunger + happiness + energy)/3
+        self.history = history
