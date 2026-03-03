62304d004ba4562d47ae3ab679cf9ef9933e5188
Shaked <shakeduzan77@gmail.com>
2026-03-03 17:52:02 +0200

    make the simple web work

--- route.py
+++ route.py
@@ -10,27 +10,27 @@ def home_page():
 @app.route('/<string:animal>/<string:name>')
 def status(name: str, animal: str):
     p = Pet(name, animal)
-    return "your pet's calculated score is: " + p.points
+    return "your pet's calculated score is: " + str(p.points)
 
 @app.route('/<string:animal>/<string:name>/hunger')
 def hunger(name: str, animal: str):
     p = Pet(name, animal)
-    return "your pet is " + p.hunger + " percent hungry"
+    return "your pet is " + str(p.hunger) + " percent hungry"
 
 @app.route('/<string:animal>/<string:name>/happiness')
 def happiness(name: str, animal: str):
     p = Pet(name, animal)
-    return "your pet is " + p.happiness + " percent happy"
+    return "your pet is " + str(p.happiness) + " percent happy"
 
 @app.route('/<string:animal>/<string:name>/energy')
 def energy(name: str, animal: str):
     p = Pet(name, animal)
-    return "your pet has " + p.energy + " percent energy"
+    return "your pet has " + str(p.energy) + " percent energy"
 
 @app.route('/<string:animal>/<string:name>/points')
 def points(name: str, animal: str):
     p = Pet(name, animal)
-    return "your pet's calculated score is: " + p.points
+    return "your pet's calculated score is: " + str(p.points)
 
 
 if __name__=='__main__':
