b973f57729a6aba9bd4c9a491a2506a8586ba431
Shaked <shakeduzan77@gmail.com>
2026-03-03 17:46:40 +0200

    write a simple route file

---
+++ route.py
@@ -0,0 +1,37 @@
+from my_first_dragon import Pet
+from flask import Flask
+
+app = Flask("my_dragon")
+
+@app.route('/')
+def home_page():
+    return "welcome to the home page!"
+
+@app.route('/<string:animal>/<string:name>')
+def status(name: str, animal: str):
+    p = Pet(name, animal)
+    return "your pet's calculated score is: " + p.points
+
+@app.route('/<string:animal>/<string:name>/hunger')
+def hunger(name: str, animal: str):
+    p = Pet(name, animal)
+    return "your pet is " + p.hunger + " percent hungry"
+
+@app.route('/<string:animal>/<string:name>/happiness')
+def happiness(name: str, animal: str):
+    p = Pet(name, animal)
+    return "your pet is " + p.happiness + " percent happy"
+
+@app.route('/<string:animal>/<string:name>/energy')
+def energy(name: str, animal: str):
+    p = Pet(name, animal)
+    return "your pet has " + p.energy + " percent energy"
+
+@app.route('/<string:animal>/<string:name>/points')
+def points(name: str, animal: str):
+    p = Pet(name, animal)
+    return "your pet's calculated score is: " + p.points
+
+
+if __name__=='__main__':
+    app.run()
