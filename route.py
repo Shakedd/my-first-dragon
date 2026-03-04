01de263d26c040cd469ce571156ec3c3705d7483
Shaked <shakeduzan77@gmail.com>
2026-03-04 17:33:01 +0200

    remove unused functions from route.py

--- route.py
+++ route.py
@@ -7,31 +7,6 @@ app = Flask("my_dragon")
 def home_page():
     return "welcome to the home page! please use the following form url to access your pet"
 
-@app.route('/<string:animal>/<string:name>/points')
-def status(name: str, animal: str):
-    p = Pet(name, animal)
-    return "your pet's calculated score is: " + str(p.points)
-
-@app.route('/<string:animal>/<string:name>/hunger')
-def hunger(name: str, animal: str):
-    p = Pet(name, animal)
-    return "your pet is " + str(p.hunger) + " percent hungry"
-
-@app.route('/<string:animal>/<string:name>/happiness')
-def happiness(name: str, animal: str):
-    p = Pet(name, animal)
-    return "your pet is " + str(p.happiness) + " percent happy"
-
-@app.route('/<string:animal>/<string:name>/energy')
-def energy(name: str, animal: str):
-    p = Pet(name, animal)
-    return "your pet has " + str(p.energy) + " percent energy"
-
-@app.route('/<string:animal>/<string:name>/points')
-def points(name: str, animal: str):
-    p = Pet(name, animal)
-    return "your pet's calculated score is: " + str(p.points)
-
 @app.route('/<string:animal>/<string:name>', methods=['GET', 'POST'])
 def index(animal, name):
     p = Pet(name, animal)
