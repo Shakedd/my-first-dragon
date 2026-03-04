55b2378b5e3777c77711b1a4bc5b0174ad3bb39d
Shaked <shakeduzan77@gmail.com>
2026-03-04 18:02:33 +0200

    add a massage after every function

--- route.py
+++ route.py
@@ -17,10 +17,13 @@ def index(animal, name):
     if request.method == 'POST':
         if request.form.get('eat') == 'EAT':
             p.eat()
+            return name + " ate!"
         if request.form.get('sleep') == 'SLEEP':
             p.sleep()
+            return name + " slept!"
         if request.form.get('play') == 'PLAY':
             p.play()
+            return name + " played!"
     return render_template('buttons.html', hunger = str(p.hunger), energy = str(p.energy), happiness = str(p.happiness), points = str(p.points))
     
 
