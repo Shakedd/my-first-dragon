319fb822260e1e0e5478f2e4174aeb2a674e9eca
Shaked <shakeduzan77@gmail.com>
2026-03-08 15:17:04 +0200

    store data in a session

--- route.py
+++ route.py
@@ -1,7 +1,12 @@
 from my_first_dragon import Pet, PetType
 from flask import Flask, request, render_template, session
+from flask_session import Session
 
 app = Flask("my_dragon")
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route('/')
 def home_page():
@@ -16,10 +21,13 @@ def index(animal, name):
     p = Pet(name, animal)
     if request.method == 'POST':
         if request.form.get('eat') == 'EAT':
+            session["eat"] = request.form.get('eat')
             p.eat()
         if request.form.get('sleep') == 'SLEEP':
+            session["sleep"] = request.form.get('sleep')
             p.sleep()
         if request.form.get('play') == 'PLAY':
+            session["play"] = request.form.get('play')
             p.play()
     return render_template('buttons.html', hunger = str(p.hunger), energy = str(p.energy), happiness = str(p.happiness), points = str(p.points), name=name, animal=animal)
     
