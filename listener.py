c4427629769125692e98773491ee792e9daf696d
Shaked <shakeduzan77@gmail.com>
2026-03-10 16:29:25 +0200

    add logging into json

---
+++ listener.py
@@ -0,0 +1,23 @@
+# import logging
+import json
+from my_first_dragon import Pet
+import functools
+
+LOG_JSON = {}
+
+# listening to the call of eat, play, sleep and log them to a json file
+def logger(func):
+    @functools.wraps(func)
+    def wrapper(p: Pet, *args, **kwargs):
+        # data = logging.basicConfig(filename="logger.json", format = '%(asctime)-15s %(levelname)-8s %(message)s',
+        #                            datefmt='%a, %d %b %Y %H:%M:%S')
+        data = {
+            "action": func.__name__,
+            "hunger": p.hunger,
+            "energy": p.energy,
+            "happiness": p.happiness}
+        LOG_JSON["%(asctime)"].append(data)
+        return func(p, *args, **kwargs)
+    return wrapper
+        
+        
